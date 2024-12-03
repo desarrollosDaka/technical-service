7ceb088f2c5c3291401aa910d29aa720f49ca777
Carmine Maggio <c.maggio@tiendasdaka.com>
2024-12-03 08:59:48 -0400

    feat: api toket authentication server

--- app/Models/Technical.php
+++ app/Models/Technical.php
@@ -3,12 +3,84 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
 
-class Technical extends Model
+class Technical extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\TechnicalFactory> */
-    use HasFactory;
+    use HasFactory, Notifiable, HasApiTokens;
 
+    /**
+     * Table name
+     *
+     * @var string
+     */
     protected $table = 'MASTER_TECHNICIAN';
+
+    /**
+     * Primary key
+     *
+     * @var string
+     */
+    protected $primaryKey = 'ID_user';
+
+    /**
+     * Timestamps
+     *
+     * @var boolean
+     */
+    public $timestamps = false;
+
+    /**
+     * Fillable
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'Name_user_comercial',
+        'Services',
+        'User_name',
+        'Email',
+        'Password',
+        'Identification_Comercial',
+        'Phone',
+        'Address',
+        'Tickets',
+        'Tickets_rejected',
+        'Qualification',
+        'Agency',
+        'ID_rol',
+        'ID_supplier',
+        'Availability',
+        'GeographicalCoordinates',
+        'Create_user',
+        'Create_date',
+        'Update_user',
+        'Update_date',
+    ];
+
+    /**
+     * Hidden element
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'Password',
+    ];
+
+    /**
+     * Casts
+     *
+     * @var array
+     */
+    protected $casts = [
+        'Password' => 'hashed',
+        'Tickets' => 'integer',
+        'Tickets_rejected' => 'integer',
+        'Qualification' => 'integer',
+        'Create_date' => 'datetime',
+        'Update_date' => 'datetime',
+    ];
 }
