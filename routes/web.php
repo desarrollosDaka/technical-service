8a473e7545a963c2e4ca46ffb7a43b2f4912c7eb
Carmine Maggio <c.maggio@tiendasdaka.com>
2024-12-04 16:33:47 -0400

    feat: new endpoint to get all tec

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,15 @@
 <?php
 
+use App\Models\Technical;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/technical', function () {
+    if (app()->environment('local')) {
+        return Technical::all();
+    }
+    return 'Oops!';
 });
