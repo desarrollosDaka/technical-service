bfdd88e4344bd96e66bd191450abb0ed9c59d25d
Carmine Maggio <c.maggio@tiendasdaka.com>
2024-12-13 15:50:35 -0400

    feat: order by

--- app/Http/Controllers/Api/V1/ProductController.php
+++ app/Http/Controllers/Api/V1/ProductController.php
@@ -14,7 +14,7 @@ class ProductController extends Controller
      */
     public function index()
     {
-        return $this->success(Product::all());
+        return $this->success(Product::orderByDesc('id')->get());
     }
 
     /**
