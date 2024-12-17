e9117db053290a2249bcaf74b62ee34d55ddc74e
Carmine Maggio <c.maggio@tiendasdaka.com>
2024-12-17 11:20:01 -0400

    feat: habilitar nueva data para observar la data del tabulador

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,9 @@
 
 use App\Http\Middleware\CurrentServiceCall;
 use App\Http\Middleware\WithoutServiceCall;
+use App\Models\Product;
 use App\Models\ServiceCall;
+use App\Models\Tabulator;
 use App\Models\Technical;
 use App\Models\Ticket;
 use Illuminate\Support\Facades\Route;
@@ -10,6 +12,7 @@ use Illuminate\Support\Facades\Route;
 Route::view('/', 'pages.index')
     ->name('index')
     ->middleware(WithoutServiceCall::class);
+
 Route::view('/show-ticket', 'pages.ticket.show')
     ->name('ticket.show')
     ->middleware(CurrentServiceCall::class);
@@ -31,6 +34,20 @@ Route::get('/service-calls', function () {
 Route::get('/tickets', function () {
     if (app()->hasDebugModeEnabled()) {
         return Ticket::orderBy('id', 'DESC')->limit(50)->get();
+    }
+    return 'Oops!';
+});
+
+Route::get('/products', function () {
+    if (app()->hasDebugModeEnabled()) {
+        return Product::orderBy('id', 'DESC')->limit(50)->get();
+    }
+    return 'Oops!';
+});
+
+Route::get('/tabulators', function () {
+    if (app()->hasDebugModeEnabled()) {
+        return Tabulator::orderBy('id', 'DESC')->limit(50)->get();
     }
     return 'Oops!';
 });
