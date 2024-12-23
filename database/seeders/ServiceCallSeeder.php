ab83cea1d0781ce9bc71df8629eed2ae39b0b8ab
Carmine Maggio <c.maggio@tiendasdaka.com>
2024-12-23 09:30:55 -0400

    feat: factory para las visitas y las part request

--- database/seeders/ServiceCallSeeder.php
+++ database/seeders/ServiceCallSeeder.php
@@ -2,8 +2,11 @@
 
 namespace Database\Seeders;
 
+use App\Models\PartRequest;
 use App\Models\ServiceCall;
 use App\Models\Technical;
+use App\Models\TechnicalVisit;
+use App\Models\Ticket;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Arr;
@@ -19,13 +22,24 @@ class ServiceCallSeeder extends Seeder
             ->count(10)
             ->create()
             ->each(function (ServiceCall $serviceCall) {
-                $serviceCall->tickets()->create([
-                    'title' => Arr::get($serviceCall, 'itemName', $serviceCall->subject),
-                    'customer_name' => $serviceCall->custmrName,
-                    'technical_id' => Technical::where('ID_user', $serviceCall->ASSIGNED_TECHNICIAN)
-                        ->first()
-                        ->getKey(),
-                ]);
+                $serviceCall
+                    ->tickets()
+                    ->create([
+                        'title' => Arr::get($serviceCall, 'itemName', $serviceCall->subject),
+                        'customer_name' => $serviceCall->custmrName,
+                        'technical_id' => Technical::where('ID_user', $serviceCall->ASSIGNED_TECHNICIAN)
+                            ->first()
+                            ->getKey(),
+                    ])
+                    ->each(function (Ticket $ticket) {
+                        TechnicalVisit::factory()
+                            ->count(10)
+                            ->create(['ticket_id' => $ticket->id])
+                            ->each(function (TechnicalVisit $visit) {
+                                // Crear solicitudes de partes para cada visita técnica
+                                PartRequest::factory()->count(10)->create(['technical_visit_id' => $visit->id,]);
+                            });
+                    });
             });
     }
 }
